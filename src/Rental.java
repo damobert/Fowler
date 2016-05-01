4624b48c6b92dbab6946d0f79049c5ab4afe1354
Damian Dochat <dochat@punkt.de>
2016-05-01 22:55:44 +0200

    [RFT] Replaces condition logic with polymorphism

--- src/Rental.java
+++ src/Rental.java
@@ -1,5 +1,5 @@
 class Rental {
-    private Movie movie;
+    Movie movie;
     private int daysRented;
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
@@ -8,35 +8,16 @@ class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+
     public Movie getMovie() {
         return movie;
     }
 
-    public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+    double getCharge(){
+        return movie.getCharge(daysRented);
     }
 
     int getFrequentRenterPoints() {
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1){
-            return 2;
-        }
-            return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
